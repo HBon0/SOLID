7fc22f6758e734adb81a8db31d58c4b00bd185e7
HBonilla <bonilla.osorio0@gmail.com>
2023-04-26 19:13:13 -0600

    Hector Bonilla: Creando Ejemplo de Abierto/Cerrado

---
+++ SOLID HLEON/SOLID/Principio_Abierto_Cerrado/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Principio_Abierto_Cerrado;
+using System.Runtime.CompilerServices;
+
+Invoice invoice = new Invoice();
+Water water = new Water();
+
+Console.WriteLine("Hello, World!");
+PrintHola();
+
+
+void PrintHola ()
+{
+    Console.WriteLine("Tu Puta madre");
+}
+
+
+public interface IDrink {
+    public string Name { get; set; }
+    public decimal Invoice { get; set; }
+    public decimal Price { get; set; }
+    public decimal GetPrice();
+}
+
+public class Water : IDrink
+{
+    public string Name { get; set; }
+    public decimal Invoice { get; set; }
+    public decimal Price { get; set; }
+    public decimal GetPrice()
+    {
+        return Price * Invoice;
+    }
+}
+
+public class Alcohol : IDrink
+{
+    public string Name { get; set; }
+    public decimal Invoice { get; set; }
+    public decimal Price { get; set; }
+    public decimal Promo { get; set; }
+    public decimal GetPrice()
+    {
+        return (Price * Invoice) - Promo;
+    }
+}
+
+public class Sugary : IDrink
+{
+    public string Name { get; set; }
+    public decimal Invoice { get; set; }
+    public decimal Price { get; set; }
+    public decimal Expiration { get; set; }
+    public decimal GetPrice()
+    {
+        return (Price * Invoice) - Expiration;
+    }
+}
+
+public class Invoice
+{
+    public decimal GetTotal(IEnumerable<IDrink> ListDrink)
+    {
+        decimal total = 0;
+        foreach(var drink in ListDrink)
+        {
+            total += drink.GetPrice();
+        }
+        return total;
+    }
+}
